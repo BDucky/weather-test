b43e5429d4ee65ca3ef627e0d44b3f0be7fa2d9b
Binhle <binh.le@quano6.com>
2023-05-24 01:26:48 +0700

    done briefly

--- src/components/form/display/index.jsx
+++ src/components/form/display/index.jsx
@@ -1,11 +1,75 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
-import data from "../../../data/data.json";
+import datas from "../../../data/data.json";
+import computerImg from "../../../assets/img/computer.png";
 
 const Display = ({ input }) => {
+  const [weather, setWeather] = useState("");
+  const [weatherStatus, setWeatherStatus] = useState("");
+  const [temp, setTemp] = useState();
+  const [pressure, setPressure] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [wind, setWind] = useState("");
+  const [country, setCountry] = useState("");
+  const [isCity, setIsCity] = useState(false)
   const api_key = "f9130498316f20bd78ac535154dd9cd3";
-  const [lat, setLat] = useState("");
-  const [lon, setLon] = useState("");
+
+  const capitalizeWords = (str) => {
+    return str
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  };
+
+  const renderError = () => {
+    return <div className="error">
+      {datas?.map(data => {
+        if (data.type === "Not Found") {
+          return (
+            <div className="error-container">
+              <div className="weather-img-container">
+                <img
+                  src={computerImg}
+                  alt={data.type}
+                  className="weather-img"
+                />
+              </div>
+              <h2 className="weather-status">{data.type}</h2>
+            </div>
+          )
+        }
+      })}
+    </div>;
+  }
+
+  const renderCityWeather = () => {
+    return (
+      <div>
+        <h1>{capitalizeWords(input)}, {country}</h1>
+        {datas?.map(data => {
+          if (data.type === weather) {
+            return (
+              <div className="weather-img-container">
+                <img
+                  src={data.img}
+                  alt={data.type}
+                  className="weather-img"
+                />
+              </div>
+            )
+          }
+        })}
+        <h2 className="weather-status">{weatherStatus.charAt(0).toUpperCase() + weatherStatus.slice(1)}</h2>
+        <h3 className="temp">{parseInt(temp).toFixed(2)}°C</h3>
+        <div className="weather-info">
+          <div className="weather-pressure">pressure: {pressure}</div>
+          <div className="weather-humidity">humidity: {humidity}</div>
+          <div className="weather-wind">wind speed: {wind}</div>
+        </div>
+      </div>
+    )
+  };
 
   useEffect(() => {
     var requestOptions = {
@@ -20,33 +84,43 @@ const Display = ({ input }) => {
       .then((response) => response.json())
       .then((result) => {
         console.log(result);
-        setLat(result[0].lat.toFixed(2));
-        setLon(result[0].lon.toFixed(2));
+        if (result.length === 0) {
+          setIsCity(false);
+        } else {
+          setIsCity(true);
+          getCurrentCity(result[0].lat, result[0].lon);
+        }
       })
       .catch((error) => console.log("error", error));
   }, [input]);
 
-  useEffect(() => {
+  const getCurrentCity = (lat, lon) => {
     var requestOptions = {
       method: "GET",
       redirect: "follow",
     };
 
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}.99&appid=${api_key}`,
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
       requestOptions
     )
-      .then((response) => response.text())
-      .then((result) => console.log("result", result))
-      .catch((error) => console.log("error", error));
-  }, [lat, lon]);
-
-  //   console.log(data);
-  console.log(lat, lon);
+      .then((response) => response.json())
+      .then((result) => {
+        setWeather(result.weather[0].main)
+        setWeatherStatus(result.weather[0].description)
+        setTemp(result.main.temp - 273.15)
+        setPressure(result.main.pressure)
+        setHumidity(result.main.humidity)
+        setWind(result.wind.speed)
+        setCountry(result.sys.country)
+      })
+      .catch((error) => renderError);
+  };
 
   return (
     <div className="display-container">
-      <h1>{input}</h1>
+      {isCity ? renderCityWeather() : renderError()}
+
     </div>
   );
 };
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -5,31 +5,36 @@ import Display from "./display";
 const MainInput = () => {
   const [input, setInput] = useState("");
 
-  const handleChange = (e) => {
-    setInput("");
-    const value = e.target.value;
-    setInput(value);
-  };
+  const searchCity = (city) => {
+    setInput(city)
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      searchCity(event.target.value)
+    }
+  }
 
   return (
     <div className="input-container">
-      <div class="navbar">
-        <div class="navbar-search">
+      <div className="navbar">
+        <div className="navbar-search">
           <div>
             <input
               type="text"
-              class="input-box"
+              className="input-box"
               placeholder="Enter your location"
-              onChange={handleChange}
+              onKeyDown={handleKeyDown}
             />
             <svg
               stroke="currentColor"
               fill="currentColor"
-              stroke-width="0"
+              strokeWidth="0"
               viewBox="0 0 16 16"
               height="1em"
               width="1em"
               xmlns="http://www.w3.org/2000/svg"
+              onClick={() => searchCity(document.querySelector(".input-box").value)}
             >
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
             </svg>
